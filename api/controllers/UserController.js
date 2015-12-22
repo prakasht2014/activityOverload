b5c79d5cf71519a2f55739b342bba3f56efea849
daeyoun24 <daeyoun1506@gmail.com>
2015-12-22 15:43:09 -0600

    Edit error control

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,7 +8,9 @@
 module.exports = {
 
 	'new': function(req, res){
+		res.locals.flash = _.clone(req.session.flash);
 		res.view();
+		req.session.flash = {};
 	},
 
 	create: function(req, res, next){
@@ -17,11 +19,19 @@ module.exports = {
 		// the sign-up form --> new.ejs
 		User.create(req.params.all(), function userCreated(err, user){
 			// If there's an error
-			if (err) return next(err);
+			if (err) {
+				console.log(err);
+				req.session.flash = {
+					err: err
+				}
+				// If error redirect back to sign-up apge
+				return res.redirect('/user/new');
+			}
 
 			// After successfully creating the user
 			// redirect to the show action
 			res.json(user);
+			req.session.flash = {};
 		});
 	}
 };
