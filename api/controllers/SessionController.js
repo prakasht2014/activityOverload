d78e5de73cb00f2356de4c99e062d9276d3bad66
Daeyoun Kim <Daeyoun Kim>
2015-12-28 16:49:28 -0600

    Deleted test codes and added create function to SessionController

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -1,13 +1,25 @@
 module.exports = {
 
-	'new': function(req, res){
-        
-        var oldDateObj = new Date();
-        var newDateObj = new Date(oldDateObj.getTime() + 60000);
-        req.session.cookie.expires = newDateObj;
-        req.session.authenticated = true;
-        console.log(req.session);
+	'new': function(req, res) {
         
         return res.view('session/new');
+	},
+	
+	'create': function(req, res, next) {
+	    
+	    // Check for email and password in params sent via the form, if none
+	    // redirect the browser back to the sign-in form.
+	    if (!req.param('email') || !req.param('password')) {
+	     
+	        var usernamePasswordRequiredError = [{name: 'usernamePasswordRequired', message: 'You must enter both a username and password.'}];
+	        
+	        // Remember that err is the object being passed down, whose value is another object with
+	        // the key of usernamePasswordRequiredError
+	        return req.session.flash = {
+	            err: usernamePasswordRequiredError
+	        };
+	        
+	        return res.redirect('/session/new');
+	    }
 	}
 };
